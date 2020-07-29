79e5215cdd114dc5721fd8a2b8123bdf1d005280
Tijela13 <62364798+Tijela13@users.noreply.github.com>
2020-07-29 12:17:11 -0300

    Add files via upload

---
+++ BlockChanger/me/tijela/BlockChanger/BlockChangerListener.java
@@ -0,0 +1,22 @@
+package me.tijela.BlockChanger;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+
+public class BlockChangerListener implements Listener
+{
+	
+	public static BlockChanger plugin;
+	
+	public BlockChangerListener(BlockChanger instance)
+	{
+		plugin = instance;
+	}
+	
+	public void onBlockDamage(BlockDamageEvent event)
+	{
+		if(plugin.enabled(event.getPlayer()))
+			event.getBlock().setTypeId(plugin.id);
+	}
+	
+}
